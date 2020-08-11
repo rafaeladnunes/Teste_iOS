9dfdd2e55867ea1c95cd4837e4a601fc7e23bcb8
Rafaela Nunes <rafaela.nunes@rarolabs.com.br>
2020-08-10 21:59:42 -0300

    images and fonts

---
+++ Teste Hotmart/Utils/Mocks/ReviewStrings.swift
@@ -0,0 +1,21 @@
+//
+//  ReviewStrings.swift
+//  Teste Hotmart
+//
+//  Created by Rafa on 10/08/20.
+//  Copyright © 2020 Rafa. All rights reserved.
+//
+
+struct ReviewStringsMock {
+    
+    static let FIRST_EVALUATION =
+    static let SECOND_EVALUATION =
+    static let THIRD_EVALUATION =
+    static let FIRST_ABOUT =
+    static let SECOND_ABOUT =
+    static let THIRD_ABOUT =
+    static let FIRST_PROFILE =
+    static let SECOND_PROFILE =
+    static let THIRD_PROFILE = 
+    
+}
